12d0b9440621b6b968abcd03efd8296c3432bd02
Michael Kilgore <64956977+MichaelKilgore@users.noreply.github.com>
2021-10-21 00:10:26 -0700

    Update pushkitTest.js

--- back-end/oldCode/pushkitTest.js
+++ back-end/oldCode/pushkitTest.js
@@ -2,16 +2,16 @@ var apn = require('apn');
 
 var options = {
   token: {
-    key: "AuthKey_92STV2HA6D.p8",
-    keyId: "92STV2HA6D",
-    teamId: "QBFL489WJL"
+    key: "",
+    keyId: "",
+    teamId: ""
   },
   production: false
 };
 
 var apnProvider = new apn.Provider(options);
 
-let deviceToken = "04f8e9df9214390d6885ab634418553b78c59cd40e02b25eddff6ef5b41cc4ba";
+let deviceToken = "";
 
 var note = new apn.Notification();
 
